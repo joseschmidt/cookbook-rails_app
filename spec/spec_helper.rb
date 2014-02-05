faf92caf1c4c9ffc2a2f7985a9fdc36662fd11ce
Doc Walker <doc.walker@jameshardie.com>
2014-02-04 21:10:53 -0800

    Fix #25 require rspec expect syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,9 @@
 require 'chef/sugar'
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end # config.expect_with
+end # RSpec

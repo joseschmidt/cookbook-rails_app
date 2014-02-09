6d39b0276bcc03db229556ada18cc432c731585a
Doc Walker <doc.walker@jameshardie.com>
2014-02-09 16:37:22 -0600

    Alphabetize dependencies

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@
 name              'rails_app'
 version           '0.3.5'
 
-depends           'cron', '~> 1.2.2'
 depends           'chef-sugar', '~> 1.1.0'
+depends           'cron', '~> 1.2.2'
 depends           'database', '~> 1.3.10'
 depends           'logrotate'
 depends           'mysql', '~> 2.1.0'
